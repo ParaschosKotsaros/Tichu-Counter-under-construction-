779f29740a42c5d7b3e31ad82c6bbb1ca229c2ce
ParaschosKotsaros <48206026+ParaschosKotsaros@users.noreply.github.com>
2021-01-09 21:58:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,29 +12,41 @@ let finalscoreB = 0;
 function addscore (){
     submit.addEventListener('click', ()=>{
     if (scoreA.value <= 125 && scoreA.value >=-25){ 
-    document.getElementById("details1").innerHTML += `<span id="details1">${scoreA.value}</span></br>`
-    document.getElementById("details2").innerHTML += `<span id="details1">${Math.abs(100 - scoreA.value)}</span></br>`
-
+      // shows the score of every round
+    document.getElementById("details1").innerHTML +=`<span class="details1">${scoreA.value}</span></br>`
+    document.getElementById("details2").innerHTML +=`<span class="details2">${Math.abs(100 - scoreA.value)}</span></br>`
     finalscoreA = finalscoreA + parseInt(scoreA.value)
     resultA.innerHTML = finalscoreA;
     finalscoreB = finalscoreB + Math.abs(100 - parseInt(scoreA.value))
     resultB.innerHTML = finalscoreB;  
+
+    winner(finalscoreA,finalscoreB)
     } else{alert("wrong score")}
     })
 }
-
-
 //announces the winner of the game 
 function winner(teamA,teamB){
-  if(teamA>1000 && teamA>teamB){
+  if(teamA>=1000 && teamA>teamB){
       alert("Team A Wins")}
-   else if (teamB >1000 && teamB > teamA){
+   else if (teamB >=1000 && teamB > teamA){
        alert("Team B Wins")}
 }
 
 // resets score back to zero for new game
 function resetscore(){
     reset.addEventListener('click',()=>{
+      //deletes the score from every round
+      if(document.getElementsByClassName("details1")){
+        document.querySelectorAll('.details1').forEach(function(a){
+        a.remove()
+        })
+      }
+      if(document.getElementsByClassName("details2")){
+        document.querySelectorAll(".details2").forEach(function(a){
+        a.remove()
+        })
+      }
+      //deletes finalscore
       resultA.innerHTML = 0;
       resultB.innerHTML = 0;
       finalscoreA = 0;
